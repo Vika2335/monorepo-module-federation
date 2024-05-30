3295ca615e5b2698ba1eceecad6fad30da110aff
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2024-05-30 20:07:16 +0300

    refactror scss module

--- services/host/src/components/App/App.tsx
+++ services/host/src/components/App/App.tsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { adminRoutes } from '@packages/shared/src/routes/admin'
 import { shopRoutes } from '@packages/shared/src/routes/shop'
-import styles from './App.module.scss';
+import '../../../../shop/src/styles/main.scss'
+import '../../../../admin/src/styles/main.scss'
+import './App.module.scss'
 
 export const App = () => {
 
   return (
     <div className="page" data-testid={'App.DataTestId'}>
       <h1>PAGE</h1>
+      <p className='title'>
+        Title
+      </p>
       <Link className="link" to={adminRoutes.about}>ABOUT</Link>
       <br/>
       <Link className="link" to={shopRoutes.main}>SHOP</Link>
